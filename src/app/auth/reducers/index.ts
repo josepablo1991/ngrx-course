208186e0a6d9a9f2046ca557210fa9f90a11e422
Jose Pablo <josepablo@gmail.com>
2022-02-21 15:43:05 +0100

    implementing the reducer

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -1,18 +1,40 @@
+import { state } from '@angular/animations';
 import {
   ActionReducer,
   ActionReducerMap,
   createFeatureSelector,
+  createReducer,
   createSelector,
-  MetaReducer
+  MetaReducer,
+  on
 } from '@ngrx/store';
+import { AuthActions } from '../action-types';
+import { User } from '../model/user.model';
 
 
 export const authFeatureKey = 'auth';
 
-export interface State {
-
+export interface AuthState {
+    user: User
 }
 
-export const reducers: ActionReducerMap<State> = {
+export const initialAuthState: AuthState = {
+  user: undefined
+}
 
-};
+
+
+// this is a reducer
+// returns a new version of the state or new stat creator
+export const authReducer = createReducer(
+  //initialState
+  initialAuthState,
+
+  on(AuthActions.login, (state:AuthState, action) => {
+    return {
+      user: action.user,
+    }
+  })
+
+
+);
